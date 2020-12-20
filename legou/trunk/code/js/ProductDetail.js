7b495d87120cf934c39dd7390fe18c6230e2dc05
txm123-bye <3108413258@qq.com>
2020-12-20 20:15:44 +0800

    猜你喜欢（左右，上下）滑动和购物车重写

--- legou/trunk/code/js/ProductDetail.js
+++ legou/trunk/code/js/ProductDetail.js
@@ -6,5 +6,36 @@ $(function(){
 		height : 469,//承载容器高
         zoom : 2//缩放比例
     })
+    /* 换一批 */	
+     //定一个索引
+	 //上下滑动版本
+	 var index = 0;
+     $('.change').click(function(){
+        //自增
+		index++;
+		//边界判断
+		index = index > 3 ? 0 :index;
+		//让里面的 inner - box 运动
+		$('.inner-box').animate({
+			top: -index * 410
+		})
+     })
+	 //左右滑动版本
+	//  var index = 0;
+	//  $('.change').click(function(){
+	//     //自增
+	//  		index++;
+	//  		//边界判断
+	//  		//让里面的 inner - box 运动
+	//  		$('.inner-box').stop(true).animate({
+	//  			left: -index * 1200
+	//  		},function(){
+	// 			if(index === 3){
+	// 				index = 0;
+	// 				$('.inner-box').css('left',0);
+	// 			}
+	// 		})
+	//  })
+
 		
 })
